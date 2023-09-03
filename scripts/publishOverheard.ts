aa639aa5a6db444063041a439ff319aa8c31fcb3
Bossett <bossett@bossett.io>
2023-09-03 22:43:26 +0000

    Updated description

--- scripts/publishOverheard.ts
+++ scripts/publishOverheard.ts
@@ -26,7 +26,7 @@ const run = async () => {
   // (Optional) A description of your feed
   // Ex: Top trending content from the whole network
   const description =
-    "A feed of posts that people have replied to, or quoted with the text 'overheard' (only)."
+    "A feed of posts that people have replied to, or quoted with just the text 'overheard'."
 
   // (Optional) The path to an image to be used as your feed's avatar
   // Ex: ~/path/to/avatar.jpeg
