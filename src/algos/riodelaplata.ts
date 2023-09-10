6f098597a4b5944bba95348060bbb3df69667967
djpeluca <adrianbs@gmail.com>
2023-09-10 20:09:15 -0300

    new feeds

---
+++ src/algos/riodelaplata.ts
@@ -0,0 +1,146 @@
+import { QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
+import { AppContext } from '../config'
+import { AlgoManager } from '../addn/algoManager'
+import dotenv from 'dotenv'
+import { Post } from '../db/schema'
+import dbClient from '../db/dbClient'
+
+dotenv.config()
+
+// max 15 chars
+export const shortname = 'riodelaplata'
+
+export const handler = async (ctx: AppContext, params: QueryParams) => {
+  const builder = await dbClient.getLatestPostsForTag(
+    shortname,
+    params.limit,
+    params.cursor,
+  )
+
+  let feed = builder.map((row) => ({
+    post: row.uri,
+  }))
+
+  let cursor: string | undefined
+  const last = builder.at(-1)
+  if (last) {
+    cursor = `${new Date(last.indexedAt).getTime()}::${last.cid}`
+  }
+
+  return {
+    cursor,
+    feed,
+  }
+}
+
+export class manager extends AlgoManager {
+  public name: string = shortname
+
+  public matchTerms: string[] = [
+    '🇦🇷',
+    '#Argentina',
+    '#TwitterArg',
+    'Argentina',
+    'Argentino',
+    'Argentinas',
+    'Argentinos',
+    'Argentinian',
+    'Argentinians',
+    'Buenos Aires',
+    'Cordoba',
+    'Malvinas',
+    'Maradona',
+    'Maradonian',
+    'Marado',
+    'el10',
+    'd10s',
+    'Puerto Madero',
+    'Peron',
+    'Kirchner',
+    'Milei',
+    '🇺🇾',
+    '#Uruguay',
+    'Uruguay',
+    'Uruguai',
+    'Uruguaya',
+    'Uruguayo',
+    'Uruguayas',
+    'Uruguayos',
+    'Uruguayan',
+    'Uruguayans',
+    'Montevideo',
+    'Punta del Este',
+    'Paysandu',
+    'Paysandú',
+    'Artigas',
+    'Rio de la Plata',
+    'tango',
+    'dulce de leche',
+  ]
+
+  public matchPatterns: RegExp[] = [
+    /(^|[\s\W])Uruguay($|[\W\s])/im,
+    /(^|[\s\W])Argentina($|[\W\s])/im,
+    /(^|[\s\W])Montevideo($|[\W\s])/im,
+    /(^|[\s\W])Buenos Aires($|[\W\s])/im,
+  ]
+
+  // Include Argentinian users here to always include their posts
+  public matchUsers: string[] = [
+    //
+  ]
+
+  // Exclude posts from these users
+  public bannedUsers: string[] = [
+    //
+  ]
+
+  public async periodicTask() {
+    await this.db.removeTagFromOldPosts(
+      this.name,
+      new Date().getTime() - 7 * 24 * 60 * 60 * 1000,
+    )
+  }
+
+  public async filter_post(post: Post): Promise<Boolean> {
+    if (post.author === 'did:plc:mcb6n67plnrlx4lg35natk2b') return false // sorry nowbreezing.ntw.app
+    if (post.replyRoot !== null) return false
+    if (this.agent === null) {
+      await this.start()
+    }
+    if (this.agent === null) return false
+
+    let match = false
+
+    let matchString = ''
+
+    if (post.embed?.images) {
+      const imagesArr = post.embed.images
+      imagesArr.forEach((image) => {
+        matchString = `${matchString} ${image.alt}`.replace('\n', ' ')
+      })
+    }
+
+    matchString = `${post.text} ${matchString}`.replace('\n', ' ')
+
+    this.matchPatterns.forEach((pattern) => {
+      if (matchString.match(pattern) !== null) {
+        match = true
+      }
+    })
+
+    this.matchTerms.forEach((term) => {
+      if (matchString.match(term) !== null) {
+        match = true
+      }
+    })
+
+    this.matchUsers.forEach((user) => {
+      if (matchString.match(user) !== null) {
+        match = true
+      }
+    })
+
+    return match
+  }
+}
