d27ca4f550b538e2a64391fef7e8339b490a6a1d
Bossett <bossett@bossett.io>
2023-09-20 01:34:48 +0000

    Removed all logging

--- src/addn/getUserDetails.ts
+++ src/addn/getUserDetails.ts
@@ -29,8 +29,6 @@ export const _getUserDetails = async (user: string, agent: BskyAgent) => {
 
     const user_details = res.data
 
-    console.log(user_details.records[0].value)
-
     return user_details.records[0].value
   } catch (error) {
     return { details: '', displayName: '' }
