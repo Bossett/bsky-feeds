740545dd254386a20c3b5fdc7b74f1511477624c
Bossett <bossett@bossett.io>
2023-11-09 20:02:20 +0000

    update description

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -26,7 +26,7 @@ const run = async () => {
   // (Optional) A description of your feed
   // Ex: Top trending content from the whole network
   const description =
-    'The Science Feed. A curated feed from Bluesky scientists, science communicators, and related experts; speaking from their expertise. Flag @danirabaiotti.bsky.social to be added or https://bsky.app/profile/bossett.bsky.social/post/3jxjfswtvil2s for more info! 🧪'
+    'The Science Feed. A curated feed from Bluesky professional scientists,  science communicators, and science/nature photographer/artists. See https://bsky.app/profile/did:plc:hu2obebw3nhfj667522dahfg/post/3kdqsp2krwq2m for more information! 🧪'
 
   // (Optional) The path to an image to be used as your feed's avatar
   // Ex: ~/path/to/avatar.jpeg
