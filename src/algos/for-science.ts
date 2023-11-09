f895ea107fc15abf7f1133f9fe1b7b72aaa6033a
Bossett <bossett@bossett.io>
2023-11-09 11:12:41 +0000

    Update pinned

--- src/algos/for-science.ts
+++ src/algos/for-science.ts
@@ -26,7 +26,7 @@ export const handler = async (ctx: AppContext, params: QueryParams) => {
   if (feed.length > 3) {
     feed.pop()
     feed.splice(1, 0, {
-      post: 'at://did:plc:jfhpnnst6flqway4eaeqzj2a/app.bsky.feed.post/3kdq2civm2f25',
+      post: 'at://did:plc:hu2obebw3nhfj667522dahfg/app.bsky.feed.post/3kdqsp2krwq2m',
     })
   }
 
