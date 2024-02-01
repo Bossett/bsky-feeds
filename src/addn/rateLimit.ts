75627872e02bd8b61985bcb8dc0080c84a0db366
Bossett <bossett@bossett.io>
2024-02-01 02:27:51 +0000

    error handling in rate limiter to avoid exceptions getting lost

--- src/addn/rateLimit.ts
+++ src/addn/rateLimit.ts
@@ -1,10 +1,19 @@
 import { pRateLimit } from 'p-ratelimit'
 
-const limit = pRateLimit({
+const _limit = pRateLimit({
   interval: 300 * 1000,
   rate: 2000,
   concurrency: 10,
   maxDelay: 30 * 1000,
 })
+
+const limit = async <T>(fn: () => Promise<T>): Promise<T> => {
+  try {
+    return await _limit(fn)
+  } catch (error) {
+    console.log(`error in limited call:\n${fn.toString()}\n${error}`)
+    throw error
+  }
+}
 
 export default limit
