256626cef9b2deed894f0b97fd3703f7cd92f696
djpeluca <adrianbs@gmail.com>
2024-02-24 02:35:16 -0300

    tidying up the repo

--- src/algos/uruguay.ts
+++ src/algos/uruguay.ts
@@ -57,6 +57,7 @@ export class manager extends AlgoManager {
     'Paysandú',
     'Artigas',
     'yorugua',
+    'U R U G U A Y'
   ]
 
   public matchPatterns: RegExp[] = [
@@ -74,12 +75,12 @@ export class manager extends AlgoManager {
     //
   ]
 
-  public async periodicTask() {
+  /*public async periodicTask() {
     await this.db.removeTagFromOldPosts(
       this.name,
       new Date().getTime() - 7 * 24 * 60 * 60 * 1000,
     )
-  }
+  }*/
 
   public async filter_post(post: Post): Promise<Boolean> {
     if (this.agent === null) {
