c76910ed2b66ef11834eb3b491891f6a051c405d
Bossett <bossett@bossett.io>
2024-09-04 20:45:34 +0000

    use public.api.bsky.app

--- src/addn/rateLimit.ts
+++ src/addn/rateLimit.ts
@@ -3,7 +3,7 @@ import { pRateLimit } from 'p-ratelimit'
 const _limit = pRateLimit({
   interval: undefined,
   rate: undefined,
-  concurrency: 48,
+  concurrency: 96,
   maxDelay: undefined,
 })
 
