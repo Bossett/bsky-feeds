8d1c46840ba52acd40bdc027fc915f8c42d48561
Bossett <bossett@bossett.io>
2024-12-31 06:17:30 +0000

    reduce retry timer

--- src/addn/rateLimit.ts
+++ src/addn/rateLimit.ts
@@ -14,7 +14,7 @@ const limit = async <T>(fn: () => Promise<T>, retries = 3): Promise<T> => {
     if (retries > 0) {
       console.log(`retrying limited call:\n${fn.toString()}`)
       const delay = (ms: number) => new Promise((res) => setTimeout(res, ms))
-      await delay(10000)
+      await delay(3000)
       return await limit(fn, retries - 1)
     } else {
       console.log(`error in limited call:\n${fn.toString()}\n${error}`)
